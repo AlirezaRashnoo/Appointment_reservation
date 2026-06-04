b8960d2b4394f57664b34471520c6c734106eff2
Alireza <alirezarashnoo85@gmail.com>
2026-06-04 20:56:02 +0330

    debuging routeHelpers

--- src/utils/routeHelpers.js
+++ src/utils/routeHelpers.js
@@ -1,10 +1,11 @@
-// routeHelpers.js
+
 export const dashboardPaths = {
-    patient: "/user-panel/details",
-    dentist: "/dentist-panel/details",
-    admin: "/admin-panel/home", // فرض کردم صفحه اصلی پنل ادمین /home است
-  };
-  
-  export function getDashboardPath(role) {
-    return dashboardPaths[role] || "/login";
-  }
+  patient: "/user-panel/details",
+  dentist: "/dentist-panel/details",
+  admin: "/admin-panel/home",
+};
+
+// تابع default export
+const getDashboardPath = (role) => dashboardPaths[role] || "/login";
+
+export default getDashboardPath;
