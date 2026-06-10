5e75ad2146aa7c356ff5a03ef342da22284e2a52
Alireza <alirezarashnoo85@gmail.com>
2026-06-10 22:13:32 +0330

    refactor: update MenuLink components to use 'to' prop for routing consistency

--- src/component/paneladmin/SidBar.jsx
+++ src/component/paneladmin/SidBar.jsx
@@ -31,7 +31,7 @@ function SidBar() {
                         </svg>
                         صفحه اصلی
                     </MenuLink>
-                    <div className="flex items-center justify-between" onClick={() => setShowSubLinks(!showSubLinks)}>
+                    <div className="flex items-center justify-between cursor-pointer" onClick={() => setShowSubLinks(!showSubLinks)}>
                         <div className="flex items-center gap-x-3">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
@@ -44,48 +44,48 @@ function SidBar() {
                     </div>
                     <ul className={`${showSubLinks?"":"hidden"} space-y-2 child:text-sm font-DanaMedium pr-8`}>
                         <li>
-                            <Link to="#">همه کاربران</Link>
+                            <Button to="users">همه کاربران</Button>
                         </li>
                         <li>
-                            <Link to="#">بیماران</Link>
+                            <Button to="users/patients-list">بیماران</Button>
                         </li>
                         <li>
-                            <Link to="#">دندانپزشکان</Link>
+                            <Button to="users/dentist-list">دندانپزشکان</Button>
                         </li>
                     </ul>
                         
                     {/* </div> */}
-                    <MenuLink  href="/registerdentist" className="flex items-center gap-x-3">
+                    <MenuLink  to="/registerdentist" className="flex items-center gap-x-3">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
                         </svg>
                         نوبت ها
                     </MenuLink>
-                    <MenuLink  href="articles" className="flex items-center gap-x-3">
+                    <MenuLink  to="articles" className="flex items-center gap-x-3">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                         </svg>
                         مقاله ها
                     </MenuLink>
-                    <MenuLink  href="comments" className="flex items-center gap-x-3">
+                    <MenuLink  to="comments" className="flex items-center gap-x-3">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
                         </svg>
                     کامنت ها
                     </MenuLink>
-                    <MenuLink  href="menous" className="flex items-center gap-x-3">
+                    <MenuLink  to="menous" className="flex items-center gap-x-3">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6.429 9.75 2.25 12l4.179 2.25m0-4.5 5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0 4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0-5.571 3-5.571-3" />
                         </svg>
                         منو ها
                     </MenuLink>
-                    <MenuLink  href="groupy" className="flex items-center gap-x-3">
+                    <MenuLink  to="groupy" className="flex items-center gap-x-3">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
                         </svg>
                     دسته بندی ها
                     </MenuLink>
-                    <MenuLink  href="#" className="flex items-center gap-x-3">
+                    <MenuLink  to="#" className="flex items-center gap-x-3">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
                         </svg>
